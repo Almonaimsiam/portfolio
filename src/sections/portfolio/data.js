77815239d133855b1286d29501bec40cb1e6a780
Al Monaim Siam <almonaim99@gmail.com>
2025-09-03 14:31:44 +0600

    initial commit

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,30 @@
+import Image1 from '../../assets/Untitled.jpg'
+import Image2 from '../../assets/Untitled2.jpg'
+import Image3 from '../../assets/project3.jpg'
+
+const data = [
+    {
+        id: 1,
+        
+        image: Image1,
+        title: "CodeShelf - Online Bookstore (Full Stack)",
+        desc: "A dedicated online store for computer science and programming books. Browse, explore, and order the resources you need in just a few clicks.Making learning easy, focused, and accessible for every tech enthusiast.",
+        demo: 'https://codeshelf-dusky.vercel.app/',
+        github: 'https://github.com/Almonaimsiam'
+    },
+    {
+        id: 2,
+       
+        image: Image2,
+        title: "PrimeTech – Tech Made Simple (Full Stack)",
+        desc: "A modern e-commerce platform designed for tech and gadget lovers. You can easily browse, compare, and shop for the latest devices. Smart shopping is quick, dependable, and fun.",
+        demo: 'http://egatortutorials.com',
+        github: 'https://github.com/Almonaimsiam'
+    },
+    
+  
+   
+]
+
+
+export default data
